ef578ef5eadc1ce6fea037ea8a660074462c7359
Michał <michalkantorski007@gmail.com>
2018-06-30 15:11:40 +0200

    Nowe

--- src/main/java/org/test/Added.java
+++ src/main/java/org/test/Added.java
@@ -3,10 +3,9 @@ package org.test;
 public class Added {
 
     public static void main (String[] args){
-        int a = 5;
         int b = 10;
         int c = 15;
-        int iloczyn = a * b*c;
+        int iloczyn = b*c;
         System.out.println("wynik mnozenia to: " + iloczyn);
 
     }
