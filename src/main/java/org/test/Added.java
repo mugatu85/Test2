b886b2be939dc908a5d106c4a3c0cddf30794b50
mugatu85 <poprawski.michal@gmail.com>
2018-06-30 14:52:04 +0200

    new commited class

--- src/main/java/org/test/Added.java
+++ src/main/java/org/test/Added.java
@@ -1,4 +1,12 @@
 package org.test;
 
 public class Added {
+
+    public static void main (String[] args){
+        int a = 5;
+        int b = 10;
+        int iloczyn = a * b;
+        System.out.println("wynik mnozenia to: " + iloczyn);
+
+    }
 }
