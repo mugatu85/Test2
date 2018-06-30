f7fb022e3b418a2e899b581ea4aa1d71700a189d
Michał <michalkantorski007@gmail.com>
2018-06-30 15:00:20 +0200

    Zmienione

--- src/main/java/org/test/Added.java
+++ src/main/java/org/test/Added.java
@@ -1,16 +1,13 @@
 package org.test;
 
 public class Added {
-    public static void main(String[] args) {
+
+    public static void main (String[] args){
         int a = 5;
         int b = 10;
-
-        int suma = a+b;
-
-
-
-        System.out.println("Wynik to " +suma);
-
+        int c = 15;
+        int iloczyn = a * b*c;
+        System.out.println("wynik mnozenia to: " + iloczyn);
 
     }
 }
