1259f35b5281c1b2d609b877a293b9cc4980e25a
Michał <michalkantorski007@gmail.com>
2018-06-30 14:51:48 +0200

    Dodawanie

--- src/main/java/org/test/Added.java
+++ src/main/java/org/test/Added.java
@@ -1,4 +1,16 @@
 package org.test;
 
 public class Added {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int suma = a+b;
+
+
+
+        System.out.println("Wynik to " +suma);
+
+
+    }
 }
